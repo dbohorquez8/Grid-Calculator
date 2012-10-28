481f2cedcb7e9f55287568c0842e8e9385bf78cf
David Bohorquez <dbohorquez8@gmail.com>
2012-10-28 18:17:04 -0500

    grid calculator, release 1.0

---
+++ application.rb
@@ -0,0 +1,65 @@
+require 'rubygems'
+require 'sinatra'
+require './environment'
+
+mime_type :ttf, "application/octet-stream"
+mime_type :woff, "application/octet-stream"
+
+configure do
+  set :views, "#{File.dirname(__FILE__)}/views"
+  set :public, "#{File.dirname(__FILE__)}/public"
+end
+
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join("\n")
+  'Application error'
+end
+
+helpers do
+  def partial(template, options = {})
+    options.merge!(:layout => false)
+    haml("shared/#{template}".to_sym, options)
+  end
+end
+
+get '/shared/:page_name' do
+  @page_name = params[:page_name]
+  haml "shared/#{@page_name}".to_sym, :layout => false
+end
+
+get '/:folder/:page_name' do
+  @page_name = params[:page_name]
+  @folder = params[:folder]
+  haml "#{@folder}/#{@page_name}".to_sym, :layout => :'layouts/main'
+end
+
+get '/:page_name' do
+  @page_name = params[:page_name]
+  haml @page_name.to_sym, :layout => :'layouts/main'
+end
+
+get '/' do
+  @page_name = 'home'
+  @wrapper_width = params[:wrapper_width].to_f
+  @gutter_width = params[:gutter_width].to_f
+
+  @wrapper_real_width = @wrapper_width - 20
+
+  @gutter_width_percent = @gutter_width*100/@wrapper_real_width
+
+  @half = (100 - @gutter_width_percent)/2
+  @third = (100 - @gutter_width_percent*2)/3
+  @quarter = (100 - @gutter_width_percent*3)/4
+  @fifth = (100 - @gutter_width_percent*4)/5
+  @sixth = (100 - @gutter_width_percent*5)/6
+
+  @twothird = @third*2 + @gutter_width_percent
+  @threequarter = @quarter*3 + @gutter_width_percent*2
+  @twofifth = @fifth*2 + @gutter_width_percent
+  @threefifth = @fifth*3 + @gutter_width_percent*2
+  @fourfifth = @fifth*4 + @gutter_width_percent*3
+  @fivesixth = @sixth*5 + @gutter_width_percent*4
+
+  haml :index, :layout => :'layouts/main'
+end
